922c05a51bb52ec4aa30824a19a30b4dec12dbc9
chasemlim <chasemlim@gmail.com>
2018-12-30 19:42:07 -0800

    Add validations for login and register

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,12 +2,23 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const keys = require('../../config/keys');
+const jwt = require('jsonwebtoken');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.get("/test", (req, res) => res.json({
     msg: "This is the users route"
 }));
 
 router.post('/register', (req, res) => {
+
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
@@ -33,6 +44,50 @@ router.post('/register', (req, res) => {
                 })
             }
         })
+})
+
+router.post('/login', (req, res) => {
+
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+        .then(user => {
+            if(!user) {
+                return res.status(404).json({ email: "This user doesn't exist" });
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        const payload = {
+                            id: user.id,
+                            handle: user.handle,
+                            email: user.email
+                        }
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: "Bearer " + token
+                                })
+                            }
+                        )
+                    } else {
+                        return res.status(400).json({ password: "Password incorrect" });
+                    }
+                })
+        })
+
 })
 
 module.exports = router;
